3c8b66ccbd052ad60e31a8dd204985096ed13dbf
se1park <swon7150@gmail.com>
2024-12-07 14:34:04 +0000

    test1에서 작업한 내용 develop-sw에 옮김

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+<<<<<<< Updated upstream
+=======
+  skip_before_action :verify_authenticity_token
+  before_action :authenticate_user!, except: [:home] # 홈 액션 제외
+>>>>>>> Stashed changes
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+<<<<<<< Updated upstream
   # 로그인
   get 'login', to: 'sessions#new', as: 'new_session'  # 로그인 폼
   post '/login', to: 'sessions#create', as: 'session'   # 로그인 처리
@@ -9,4 +10,27 @@ Rails.application.routes.draw do
 
   # 루트 경로
   root 'mains#index'
+=======
+  get 'pages/home'
+
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  root 'pages#home'
+
+  resources :videos do
+    member do
+      patch :like, to: "video_reactions#like"
+      patch :dislike, to: "video_reactions#dislike"
+    end
+    collection do
+      get :search, to: "videos#search" # 검색 라우트 추가
+    end
+
+    # 댓글 생성 라우트 추가
+    resources :comments, only: [:create]
+  end
+
+  # 회원가입
+  resources :mains
+  resources :categories, only: [:create]
+>>>>>>> Stashed changes
 end
